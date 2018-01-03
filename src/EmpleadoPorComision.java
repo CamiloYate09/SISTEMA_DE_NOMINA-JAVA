c9f354e5128de0eb9cb0129605edaa9066a0af33
Kernel-2018 <Kernel-2018@Kernel-2018-PC>
2018-01-03 17:55:19 -0500

    sistema de nomina con uml

---
+++ src/EmpleadoPorComision.java
@@ -0,0 +1,49 @@
+
+public class EmpleadoPorComision extends Empleado{
+    
+    private double ventasBrutas;
+    private double tarifaComision;
+    
+    public EmpleadoPorComision(String nombre, String apellido, String nss, double ventas, double tarifa){
+        super(nombre, apellido, nss);
+        
+        establecerVentasBrutas(ventas);
+        establecerTarifaComision(tarifa);
+    }
+    
+    public void establecerTarifaComision(double tarifa) {
+        if(tarifa > 0.0 && tarifa < 1.0 ){
+            tarifaComision = tarifa;
+        }
+        else{
+            tarifa = 0.0;
+        }
+    }
+    
+    public double obtenerTarifaComision(){
+        return tarifaComision;
+    }
+    public void establecerVentasBrutas(double ventas){
+        if(ventas < 0.0){
+            ventasBrutas = 0.0;
+        }
+        else {
+            ventasBrutas = ventas;
+        }
+    }
+    
+    public double obtenerVentasBrutas(){
+        return ventasBrutas;
+    }
+    
+    @Override
+    public double ingresos(){
+        return obtenerTarifaComision() * obtenerVentasBrutas();
+    }
+    
+    @Override
+    public String toString(){
+        return String.format("%s: %s\n%s: $%,.2f; %s: %.2f", "empleado por comisión", super.toString(), "ventas brutas", obtenerVentasBrutas(),
+                "tarifia de comisión", obtenerTarifaComision());
+    }
+}
---
+++ src/EmpleadoPorHoras.java
@@ -0,0 +1,58 @@
+
+public class EmpleadoPorHoras extends Empleado{
+    
+    private double sueldo;  //sueldo por hora
+    private double horas;    //horas trabajadas.
+    
+    public EmpleadoPorHoras(String nombre, String apellido, String nss, double salarioPorHoras, double horasTrabajadas){
+        super(nombre, apellido, nss);
+        
+        establecerSueldo(salarioPorHoras);
+        establecerHoras(horasTrabajadas);
+    }
+
+    public double obtenerSueldo() {
+        return sueldo;
+    }
+
+    public void establecerSueldo(double su) {
+        if(su < 0.0) {
+            sueldo = 0.0;
+        }else{
+            sueldo = su;
+        }
+        
+    }
+
+    public double obtenerHoras() {
+        return horas;
+    }
+
+    public void establecerHoras(double h) {
+        if((h >= 0) && (h <=168)){
+            horas = h;
+        }
+        else{
+           horas = 0;
+        }
+    }
+    
+    @Override 
+    public double ingresos(){
+        if(obtenerHoras()<= 40){
+            return obtenerSueldo() * obtenerHoras();
+        }
+        else{
+            return 40 * obtenerSueldo() + (obtenerHoras() - 40) * obtenerSueldo() * 1.5;
+        }
+    }
+    
+    @Override
+    public String toString(){
+        return String.format("empleado por horas: %s\n%s: $%,.2f; %s: %,.2f", super.toString(), "sueldo por hora", obtenerSueldo(), "horas trabajadas",
+                obtenerHoras());
+    }
+    
+    
+    
+}
